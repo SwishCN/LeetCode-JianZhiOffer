092a8dc9cbfcc7275122dd25ccbdf0233d13507a
SwishCN <swh524457@163.com>
2021-04-10 13:31:45 +0800

    “LeetCode剑指Offer18题解”

---
+++ LCOffer18.java
@@ -0,0 +1,46 @@
+/**
+ * 剑指 Offer 18. 删除链表的节点
+ * 给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
+ * 返回删除后的链表的头节点。
+ *
+ * 示例 1:
+ * 输入: head = [4,5,1,9], val = 5
+ * 输出: [4,1,9]
+ * 解释: 给定你链表中值为5的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+ * 示例 2:
+ * 输入: head = [4,5,1,9], val = 1
+ * 输出: [4,5,9]
+ * 解释: 给定你链表中值为1的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    public ListNode deleteNode(ListNode head, int val) {
+
+        if (head == null) {
+            return null;
+        }
+
+        if (head.val == val) {
+            return head.next;
+        }
+
+        ListNode current = head;
+        while (current.next != null && current.next.val != val) {
+            current = current.next;
+        }
+
+        if (current.next != null) {
+            current.next = current.next.next;
+        }
+        return head;
+    }
+
+}
