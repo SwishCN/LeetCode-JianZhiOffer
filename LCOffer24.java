36237b5c5c0cb9055d16524e9617872df6213e81
SwishCN <swh524457@163.com>
2021-04-13 19:39:24 +0800

    “LeetCode剑指Offer24题解”

---
+++ LCOffer24.java
@@ -0,0 +1,36 @@
+/**
+ * 剑指 Offer 24. 反转链表
+ * 定义一个函数，输入一个链表的头节点，
+ * 反转该链表并输出反转后链表的头节点。
+ *
+ * 示例:
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ *
+ * 限制：
+ * 0 <= 节点个数 <= 5000
+ *
+ * 思路：https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof/solution/jian-zhi-offer-24-fan-zhuan-lian-biao-die-dai-di-2/
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head, pre = null;
+
+        while(cur != null) {
+            ListNode tmp = cur.next; // 暂存后继节点 cur.next
+            cur.next = pre;          // 修改 next 引用指向
+            pre = cur;               // pre 暂存 cur
+            cur = tmp;               // cur 访问下一节点
+        }
+
+        return pre;
+    }
+}
